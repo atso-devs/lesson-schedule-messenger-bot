c86389bdec83a9dd07b9f34d8979064b08721fee
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-10-16 23:55:46 +0600

    wip(bot): change peer_id

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ const axios = require('axios'),
 module.exports = {
     sendMessage: async (message, groupId) => {
         const data = {
-            peer_id: 2000000000 + groupId,
+            peer_id: message.peer_id,
             access_token: accessToken,
             random_id: Math.floor(Math.random() * 20000000000),
             message: message.text,
