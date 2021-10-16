92afb1ae40018e73ac85b3319cc1b4cb395fbfdd
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-10-16 19:23:30 +0600

    wip(bot): vk-api post-body

--- src/serviceHandler.js
+++ src/serviceHandler.js
@@ -3,6 +3,8 @@ const messageService = require('./messageService')
 module.exports = async function handle(body) {
     if (body.type === 'confirmation') {
         return process.env.vk_confirmation_string
-    } else
+    } else {
+        console.log(body)
         return await messageService.testServiceMethod(body.message)
+    }
 }
