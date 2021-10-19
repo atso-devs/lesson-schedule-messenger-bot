a541d372ae6b039d5d67d00b3326e4a7bf0f5d7a
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-10-19 18:15:38 +0600

    fix(server): change check message field exist

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const app = require('express')(),
 app.use(bodyParser.json())
 
 app.post('/', catchError(async (req, res, next) => {
-    if (!(req.body.object.message.text.startsWith('/') && req.body.object.message)) {
+    if (!req.body.object.message) {
+        return
+    }
+    if (!req.body.object.message.text.startsWith('/')) {
         return
     }
     const result = await serviceHandler(req.body)
