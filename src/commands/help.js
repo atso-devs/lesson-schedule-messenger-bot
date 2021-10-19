4b477b58a225c527617c2373bfe1e68a8a6b4039
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-10-19 20:00:36 +0600

    add(server): help command message

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,9 +1,19 @@
 const api = require('../api')
 
+function createHelpMessage() {
+    const commands = require('./commands.js')
+    let result = `Все доступные команды:\n`
+
+    Object.keys(commands).forEach(command => {
+        result += `/${commands[command].name} - ${commands[command].description}\n`
+    })
+    return `${result}\n\nДанный список будет дополняться`
+}
+
 module.exports = {
     name: 'help',
     description: 'Выводит все доступные команды',
     execute: async (peerId) => {
-        await api.sendMessage('help (wip)', peerId)
+        await api.sendMessage(createHelpMessage(), peerId)
     }
 }
