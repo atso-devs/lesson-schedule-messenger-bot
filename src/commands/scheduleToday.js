338778b36df4f81381a0f1414f27ede2af968fbb
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-10-20 12:01:29 +0600

    fix(scheduleToday): date timeZone

--- src/commands/scheduleToday.js
+++ src/commands/scheduleToday.js
@@ -1,4 +1,5 @@
-const api = require('../api')
+const api = require('../api'),
+    timeZone = 6*60*60*1000
 
 function createLessonString(lesson) {
     const common = `${lesson.begins}-${lesson.ends} - ${lesson.name}.`
@@ -13,13 +14,14 @@ module.exports = {
     name: 'lessons',
     description: 'Выводит расписание на сегодня',
     execute: async (peerId, date) => {
-        const weekDay = new Date(date*1000).getDay()-1
+        const weekDay = new Date(date*1000+timeZone).getDay()-1
         const schedule = (await api.getSchedule())[weekDay>4 ? 0 : weekDay],
              lessonList = schedule.lessons.map(lesson => { return createLessonString(lesson) })
 
         const scheduleMessage = `${schedule.dayName}\n
             ${lessonList.join('\n')} 
         `
+        console.log(scheduleMessage)
 
         await api.sendMessage(scheduleMessage, peerId)
     }
