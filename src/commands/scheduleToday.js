0d7562c09aa1fcdc0ea8fe6acc1a3b2d3596a5d8
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-10-24 01:37:18 +0600

    fix(scheduleToday): weekend day not answering

--- src/commands/scheduleToday.js
+++ src/commands/scheduleToday.js
@@ -1,5 +1,6 @@
 const api = require('../api'),
-    timeZone = process.env.time_zone*60*60*1000
+    timeZone = process.env.time_zone*60*60*1000,
+    CommandError = require('../CommandError')
 
 function createLessonString(lesson) {
     const common = `${lesson.begins}-${lesson.ends} - ${lesson.name}.`
@@ -15,13 +16,14 @@ module.exports = {
     description: 'Выводит расписание на сегодня',
     execute: async (peerId, date, args) => {
         if (args.length > 1) {
-            await api.sendMessage('У этой команды должно быть 0 или 1 аргумент', peerId)
-            return
+            throw new CommandError('У этой команды должно быть 0 или 1 аргумент', peerId)
         }
 
         if (args.length === 0) {
             const weekDay = new Date(date*1000+timeZone).getDay()-1
-            const schedule = (await api.getSchedule())[weekDay>4 ? 0 : weekDay],
+            console.log(weekDay)
+            console.log(weekDay>4 ? 0 : weekDay)
+            const schedule = (await api.getSchedule())[weekDay>4 ? 0 : weekDay<0 ? 0 : weekDay],
                 lessonList = schedule.lessons.map(lesson => { return createLessonString(lesson) })
 
             const scheduleMessage = `${schedule.dayName}\n\n${lessonList.join('\n\n')}`
@@ -34,14 +36,15 @@ module.exports = {
         const indexOfDay = week.indexOf(args[0].toLowerCase())
 
         if (indexOfDay === -1) {
-            await api.sendMessage(`Неизвестный день недели или этот день выходной: '${args[0].trim()}'`, peerId)
-        } else {
-            const schedule = (await api.getSchedule())[indexOfDay],
-                lessonList = schedule.lessons.map(lesson => { return createLessonString(lesson) })
-
-            const scheduleMessage = `${schedule.dayName}\n\n${lessonList.join('\n\n')}`
-
-            await api.sendMessage(scheduleMessage, peerId)
+            throw new CommandError(`Неизвестный день недели или этот день выходной: '${args[0].trim()}'`, peerId)
         }
+        
+        const schedule = (await api.getSchedule())[indexOfDay],
+            lessonList = schedule.lessons.map(lesson => { return createLessonString(lesson) })
+
+        const scheduleMessage = `${schedule.dayName}\n\n${lessonList.join('\n\n')}`
+
+        await api.sendMessage(scheduleMessage, peerId)
+
     }
 }
