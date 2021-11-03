95e1f644ee5adb84dc8aa9ecba22a1b3211492b9
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2021-11-03 15:18:11 +0600

    fix(cron): weekday (wip)

--- src/cron.js
+++ src/cron.js
@@ -5,7 +5,7 @@ const scheduleKeys = ['Понедельник', 'Вторник', 'Среда', 
 
 
 function createLessonString(lesson) {
-    const common = `Пара через 10 минут\n\n${lesson.begins}-${lesson.ends} - ${lesson.name}.`
+    const common = `@all, пара через 10 минут\n\n${lesson.begins}-${lesson.ends} - ${lesson.name}.`
     if (lesson.type === 'zoom') {
         return `${common} ID-конференции: ${lesson.zoomId}. Пароль: ${lesson.zoomPassword}. ${lesson.lecturer}`
     }
@@ -13,15 +13,18 @@ function createLessonString(lesson) {
 }
 
 async function makeLessonQueue() {
-    let weekDay = new Date(Date.now()+timeZone).getDay()
+    let weekDay = new Date(Date.now()+timeZone).getDay()-1
+    console.log(weekDay)
 
-    weekDay = weekDay>4 ? 0 : weekDay
+    weekDay = weekDay>4 ? 0 : weekDay<0 ? 0 : weekDay
     const schedule = (await api.getSchedule()).get(scheduleKeys[weekDay])
+    if (!schedule) { return }
     schedule.map( lesson => {
         const [hours, minutes] = lesson.begins.split(':')
 
         const now = new Date(Date.now())
-        const date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), +hours, minutes-10, 0, 0)
+        const date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours-process.env.time_zone, minutes-10, 0, 0)
+        console.log('notification will be in: ', date.toString())
         setTimeout(async () => {
             await api.sendMessage(createLessonString(lesson), 231237066)
 
@@ -33,10 +36,10 @@ async function makeLessonQueue() {
 module.exports = {
     start: async () => {
         await makeLessonQueue()
-            const now = new Date()
+        const now = new Date(),
+            start = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 0, 0, 0, 0),
+            wait = start.getTime() - now.getTime()
 
-        const start = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 0, 0, 0, 0)
-        let wait = start.getTime() - now.getTime()
         console.log('Time now: ' + new Date(Date.now()).toString())
         console.log('Start in: ' + new Date(Date.now()+wait).toString())
         setTimeout( () => {
